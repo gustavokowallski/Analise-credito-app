ab5ce1eb8f56a66a53486c2f09debe3b5a0ce8e8
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-08-27 01:29:44 -0300

    feat: Adiciona service para Notificar RabbitMq

--- analisecredito/src/main/java/com/pieropan/analisecredito/service/AnalysisCreditService.java
+++ analisecredito/src/main/java/com/pieropan/analisecredito/service/AnalysisCreditService.java
@@ -3,24 +3,32 @@ package com.pieropan.analisecredito.service;
 import com.pieropan.analisecredito.domain.Proposal;
 import com.pieropan.analisecredito.exceptions.StrategyException;
 import com.pieropan.analisecredito.service.strategy.ScoreCalculation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AnalysisCreditService {
+
+    @Autowired
     private List<ScoreCalculation> scoreCalculationList;
 
-    public AnalysisCreditService(List<ScoreCalculation> scoreCalculationList){
-        this.scoreCalculationList = scoreCalculationList;
-    }
+    @Value("{rabbitmq.exchange.completedproposal}")
+    private String exchangeProposalConcluded;
+
+    @Autowired
+    private NotificationRabbitMQService notificacaoRabbitMQService;
+
     public void toAnalyze(Proposal proposal){
         try {
-            boolean approved = scoreCalculationList.stream().mapToInt(impl -> impl.calculate(proposal)).sum() > 350;
-            proposal.setApproved(approved);
+            int pontos = scoreCalculationList.stream().mapToInt(impl -> impl.calculate(proposal)).sum();
+            proposal.setApproved(pontos > 350);
         } catch (StrategyException e) {
             proposal.setApproved(false);
+            proposal.setObservation(e.getMessage());
         }
-
+        notificacaoRabbitMQService.toNotify(exchangeProposalConcluded, proposal);
     }
 }
